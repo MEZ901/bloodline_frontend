cb4f48dc223e4b407d591511fa4bfe5f6e734ded
issam <issammez44@gmail.com>
2023-04-03 00:47:12 +0200

    update the register UI

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,29 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Logo } from "../../assets";
 import { TextField, InputAdornment, IconButton, FormControl, OutlinedInput, InputLabel, Button } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { useState } from "react";
+import { Logo } from "../../assets";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   return (
-    <section class="bg-gray-50">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <Link to="/" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-                <img class="w-16 h-16" src={Logo} alt="logo" />
+    <section className="bg-gray-50">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                <img className="w-16 h-16" src={Logo} alt="logo" />
             </Link>
-            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+            <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                         Sign in to your account
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    <form className="space-y-4 md:space-y-6" action="#">
                         <div>
                             <TextField
                                 id="email"
                                 name="email"
-                                label="email"
+                                label="Email"
                                 variant="outlined"
                                 fullWidth
                             />
@@ -50,27 +50,26 @@ const Login = () => {
                             />
                         </FormControl>
                         </div>
-                        <div class="flex items-center justify-between">
-                            <div class="flex items-start">
-                                <div class="flex items-center h-5">
-                                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 cursor-pointer" required="" />
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 cursor-pointer" required="" />
                                 </div>
-                                <div class="ml-3 text-sm">
-                                    <label htmlFor="remember" class="text-gray-500 cursor-pointer">Remember me</label>
+                                <div className="ml-3 text-sm">
+                                    <label htmlFor="remember" className="text-gray-500 cursor-pointer">Remember me</label>
                                 </div>
                             </div>
-                            <Link to="#" class="text-sm font-medium text-primary-600 hover:underline">Forgot password?</Link>
+                            <Link to="#" className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</Link>
                         </div>
                         <Button type="submit" variant="contained" style={{ backgroundColor: '#FF1C23' }} fullWidth>Sign in</Button>
-                        <p class="text-sm font-light text-gray-500">
-                            Don’t have an account yet? <Link to="/register" class="font-medium text-primary hover:underline">Sign up</Link>
+                        <p className="text-sm font-light text-gray-500">
+                            Don’t have an account yet? <Link to="/register" className="font-medium text-primary hover:underline">Sign up</Link>
                         </p>
                     </form>
                 </div>
             </div>
         </div>
-        </section>
-
+    </section>
   );
 }
 
--- src/features/auth/Register.jsx
+++ src/features/auth/Register.jsx
@@ -1,103 +1,131 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { TextField, InputAdornment, IconButton, FormControl, OutlinedInput, InputLabel, Button, Autocomplete, Checkbox, FormControlLabel } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Logo } from "../../assets";
 
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme();
-
-export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-
+const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleClickShowPasswordConfirmation = () => setShowPasswordConfirmation((show) => !show);
+  const bloodTypes = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-', 'I don\'t know'];
   return (
-    <>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </>
+    <section className="bg-gray-50">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:min-h-screen lg:py-0">
+            <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                <img className="w-16 h-16" src={Logo} alt="logo" />
+            </Link>
+            <div className="w-full bg-white rounded-lg shadow my-2 md:mt-0 sm:max-w-md xl:p-0">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                        Create new account
+                    </h1>
+                    <form className="space-y-4 md:space-y-6" action="#">
+                        <div className="flex gap-2">
+                            <TextField
+                                id="first_name"
+                                name="first_name"
+                                label="First Name"
+                                variant="outlined"
+                            />
+                            <TextField
+                                id="last_name"
+                                name="last_name"
+                                label="Last Name"
+                                variant="outlined"
+                            />
+                        </div>
+                        <div className="flex gap-2">
+                            <TextField
+                                id="cin"
+                                name="cin"
+                                label="CIN"
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <Autocomplete
+                                disablePortal
+                                id="bloodType"
+                                name="bloodType"    
+                                options={bloodTypes}
+                                fullWidth
+                                renderInput={(params) => <TextField {...params} label="Blood Types" />}
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                id="email"
+                                name="email"
+                                label="Email"
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </div>
+                        <div>
+                            <FormControl fullWidth variant="outlined">
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <OutlinedInput
+                                    id="password"
+                                    name="password"
+                                    label="Password"
+                                    type={showPassword ? 'text' : 'password'}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                edge="end"
+                                            >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                        </div>
+                        <div>
+                            <FormControl fullWidth variant="outlined">
+                                <InputLabel htmlFor="passwordConfirmation">Confirm Password</InputLabel>
+                                <OutlinedInput
+                                    id="passwordConfirmation"
+                                    name="passwordConfirmation"
+                                    label="Confirm Password"
+                                    type={showPasswordConfirmation ? 'text' : 'password'}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPasswordConfirmation}
+                                                edge="end"
+                                            >
+                                            {showPasswordConfirmation ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 cursor-pointer" required="" />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label htmlFor="remember" className="text-gray-500 cursor-pointer">I accept the <Link to="#" className="font-medium text-primary hover:underline">Terms and Conditions</Link></label>
+                                </div>
+                            </div>
+                        </div>
+                        <Button type="submit" variant="contained" style={{ backgroundColor: '#FF1C23' }} fullWidth>Sign in</Button>
+                        <p className="text-sm font-light text-gray-500">
+                            Already have an account? <Link to="/login" className="font-medium text-primary hover:underline">Sign in</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
   );
 }
+
+export default Register;
