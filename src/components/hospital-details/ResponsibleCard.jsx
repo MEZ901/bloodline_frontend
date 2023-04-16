75514912b34683889f8e4ab98df64636d8895e65
issam <issammez44@gmail.com>
2023-04-16 13:39:26 +0000

    destracting props in Responsible card and hospital details

--- src/components/hospital-details/ResponsibleCard.jsx
+++ src/components/hospital-details/ResponsibleCard.jsx
@@ -1,23 +1,23 @@
 import { Profile } from "../../assets";
 
-const ResponsibleCard = ({ responsible }) => {
+const ResponsibleCard = ({ firstName, lastName, profileImage, phone }) => {
   return (
     <div className="flex justify-center">
       <div className="text-center bg-glass p-5 rounded transition-all">
         <div
           className="mx-auto mb-4 w-36 h-36 rounded-full"
           style={{
-            backgroundImage: `url('${responsible.profile || Profile}')`,
+            backgroundImage: `url('${profileImage || Profile}')`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
           }}
         />
         <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900">
-          <p>{`${responsible.firstName} ${responsible.lastName}`}</p>
+          <p>{`${firstName} ${lastName}`}</p>
         </h3>
         <p>Responsible</p>
-        <p>{responsible.phone}</p>
+        <p>{phone}</p>
       </div>
     </div>
   );
--- src/features/hospitals/HospitalDetails.jsx
+++ src/features/hospitals/HospitalDetails.jsx
@@ -1,17 +1,17 @@
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectHospitalById } from "./hospitalSelectors";
-import { ScheduleAppointment } from "../../assets";
+import dayjs from "dayjs";
 import { Chip } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { StaticDateTimePicker } from "@mui/x-date-pickers";
-import dayjs from "dayjs";
+import { selectHospitalById } from "./hospitalSelectors";
+import { ScheduleAppointment } from "../../assets";
 import { ResponsibleCard } from "../../components/hospital-details";
 
 const HospitalDetails = () => {
   const { id } = useParams();
-  const hospital = useSelector((state) =>
-    selectHospitalById(state, Number(id))
+  const { name, city, geographicCoordinate, responsible } = useSelector(
+    (state) => selectHospitalById(state, Number(id))
   );
 
   const disableWeekends = (date) => {
@@ -27,19 +27,21 @@ const HospitalDetails = () => {
     return disabledHours.includes(hour);
   };
 
-  const GeographicCoordinate =
-    "!1m18!1m12!1m3!1d15371.38079613829!2d-8.523328005249686!3d32.244553358684065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdaefdd4fcbbdbc1%3A0x846cbd9f328a7bdb!2sYoucode!5e0!3m2!1sen!2sma!4v1681518410236!5m2!1sen!2sma";
+  const handleChange = (newValue) => {
+    const dateAndTimeISO = dayjs(newValue).toISOString();
+    console.log(dateAndTimeISO);
+  };
 
   return (
     <div>
       <div className="flex flex-col sm:flex-row sm:justify-between my-3">
-        <h3 className="text-3xl font-bold">{hospital.name}</h3>
-        <Chip icon={<LocationOnIcon />} label={hospital.city} />
+        <h3 className="text-3xl font-bold">{name}</h3>
+        <Chip icon={<LocationOnIcon />} label={city} />
       </div>
       <div className="my-5">
         <div className="text-lg text-gray-600">
           <iframe
-            src={`https://www.google.com/maps/embed?pb=${hospital.geographicCoordinate}`}
+            src={`https://www.google.com/maps/embed?pb=${geographicCoordinate}`}
             width="100%"
             height="450"
             style={{ border: 0 }}
@@ -61,7 +63,8 @@ const HospitalDetails = () => {
             shouldDisableDate={disableWeekends}
             shouldDisableTime={shouldDisableTime}
             defaultValue={dayjs(new Date())}
-            label="Date & Time"
+            onChange={handleChange}
+            onAccept={() => console.log("hh")}
           />
           <img
             src={ScheduleAppointment}
@@ -71,7 +74,7 @@ const HospitalDetails = () => {
           />
         </div>
         <div className="my-10">
-          <ResponsibleCard responsible={hospital.responsible} />
+          <ResponsibleCard {...responsible} />
         </div>
       </div>
     </div>
